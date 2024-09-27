0658e41b46841e54924ae639e32149fa7e34051b
Satorki <sebastian.kedzier@gmail.com>
2024-09-27 12:58:23 +0200

    unused imports deleted

--- components/atoms/AboutImage.tsx
+++ components/atoms/AboutImage.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import Reparing from "../../public/images/repairing.jpg";
 
