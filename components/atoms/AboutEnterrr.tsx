1fb220dc85706f8f32bbeb840bc360e1a863d599
Satorki <sebastian.kedzier@gmail.com>
2024-09-28 11:02:47 +0200

    about car opacity added

---
+++ components/atoms/AboutEnterrr.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import car from "../../public/images/garage-car.png";
+import nocar from "../../public/images/garage-nocar.png";
+import Image from "next/image";
+
+const AboutEnterrr = () => {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+
+  const carOpacity = useTransform(scrollYProgress, [0, 2], [1, 0]);
+  const nocarOpacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
+
+  return (
+    <div className="relative -mt-[90px]" ref={container}>
+      <div className="h-[300vh] w-full">
+        <motion.div className="w-full h-screen z-20" style={{}}>
+          <Image src={nocar} alt="nocar" layout="fill" objectFit="cover" />
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutEnterrr;
--- containers/home-page/About.tsx
+++ containers/home-page/About.tsx
@@ -3,24 +3,30 @@ import AboutImage from "@/components/atoms/AboutImage";
 import AboutQuote from "@/components/atoms/AboutQuote";
 import React from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
+import AboutEnter from "@/components/molecules/AboutEnter";
+import AboutEnterrr from "@/components/atoms/AboutEnterrr";
 
 const About = () => {
   const { scrollYProgress } = useScroll();
   const xPos = useTransform(scrollYProgress, [0, 1], [2000, 0]);
 
   return (
-    <div
-      id="About"
-      style={{ marginTop: "-60px" }}
-      className="grid grid-cols-1 md:grid-cols-2 gap-4 grid-flow-col items-center overflow-hidden"
-    >
-      <motion.div initial={{ x: -500 }} style={{ x: xPos }}>
-        <p className="absolute top-0 text-white font-mono font-2xl font-bold">
-          CAR
-        </p>
-      </motion.div>
-      <AboutImage />
-      <AboutQuote />
+    <div>
+      <AboutEnter />
+      <AboutEnterrr />
+      <div
+        id="About"
+        style={{ marginTop: "-60px" }}
+        className="grid grid-cols-1 md:grid-cols-2 gap-4 grid-flow-col items-center overflow-hidden"
+      >
+        {/* <motion.div initial={{ x: -500 }} style={{ x: xPos }}>
+          <p className="absolute top-0 text-white font-mono font-2xl font-bold">
+            CAR
+          </p>
+        </motion.div>
+        <AboutImage />
+        <AboutQuote /> */}
+      </div>
     </div>
   );
 };
