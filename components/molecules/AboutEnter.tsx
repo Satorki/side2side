e89819a8a89c4bb4b3993cc96cf9d04eb659d394
Satorki <sebastian.kedzier@gmail.com>
2024-09-28 12:32:23 +0200

    not usable about enter component added

--- components/molecules/AboutEnter.tsx
+++ components/molecules/AboutEnter.tsx
@@ -6,34 +6,35 @@ import nocar from "../../public/images/garage-nocar.png";
 import Image from "next/image";
 
 const AboutEnter = () => {
-
   const container = useRef(null);
   const { scrollYProgress } = useScroll({
     target: container,
     offset: ["start start", "end end"],
   });
 
-  const carOpacity = useTransform(scrollYProgress, [0, 2], [1, 0]);
-  const nocarOpacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
+  const carOpacity = useTransform(scrollYProgress, [0, 0.1], [1, 0]);
+  const garageOpacity = useTransform(scrollYProgress, [0, 0.8], [1, 0]);
+  const zoomIn = useTransform(scrollYProgress, [0, 5], [1, 5]);
 
   return (
     <div className="relative -mt-[90px]" ref={container}>
-      <div className="h-[300vh] w-full">
+      <div className="w-full">
+        <motion.div
+          className="w-full h-screen"
+          style={{
+            position: "sticky",
+            inset: 0,
+            zoom: zoomIn,
+            opacity: garageOpacity,
+          }}
+        >
+          <Image src={nocar} alt="nocar" layout="fill" objectFit="cover" />
+        </motion.div>
         <motion.div
           style={{ position: "sticky", inset: 0, opacity: carOpacity }}
           className="top-[70px] w-full h-screen z-10"
         >
           <Image src={car} alt="car" layout="fill" objectFit="cover" />
-        </motion.div>
-        <motion.div
-          className="w-full h-screen z-20"
-          style={{
-            position: "sticky",
-            inset: 0,
-            opacity: nocarOpacity,
-          }}
-        >
-          <Image src={nocar} alt="nocar" layout="fill" objectFit="cover" />
         </motion.div>
       </div>
     </div>
--- containers/home-page/About.tsx
+++ containers/home-page/About.tsx
@@ -4,29 +4,28 @@ import AboutQuote from "@/components/atoms/AboutQuote";
 import React from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import AboutEnter from "@/components/molecules/AboutEnter";
-import AboutEnterrr from "@/components/atoms/AboutEnterrr";
 
 const About = () => {
   const { scrollYProgress } = useScroll();
   const xPos = useTransform(scrollYProgress, [0, 1], [2000, 0]);
 
   return (
-    <div>
+    <div className="bg-black">
       <AboutEnter />
-      <AboutEnterrr />
-      <div
+
+      {/* <div
         id="About"
         style={{ marginTop: "-60px" }}
         className="grid grid-cols-1 md:grid-cols-2 gap-4 grid-flow-col items-center overflow-hidden"
       >
-        {/* <motion.div initial={{ x: -500 }} style={{ x: xPos }}>
+        <motion.div initial={{ x: -500 }} style={{ x: xPos }}>
           <p className="absolute top-0 text-white font-mono font-2xl font-bold">
             CAR
           </p>
         </motion.div>
         <AboutImage />
-        <AboutQuote /> */}
-      </div>
+        <AboutQuote />
+      </div> */}
     </div>
   );
 };
