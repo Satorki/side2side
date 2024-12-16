cf97941a4d9ec027490707d1127c7d6343415be4
Satorki <sebastian.kedzier@gmail.com>
2024-12-16 13:46:03 +0100

    title and description for layout added, opengraph image added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,18 @@ import Header from "@/components/organisms/Header";
 import Favicon from "@/public/images/Icons/favicon-bigs.png";
 
 export const metadata: Metadata = {
-  title: "Side2Side",
-  description: "Side2Side Profesjonalny Serwis, Profesjonalny Tuning, Obsługa Motorsport",
+  title: "Side2Side | Eksperci w serwisie, tuningu i motorsporcie",
+  description:
+    "Side2Side to profesjonalny warsztat samochodowy, który łączy pasję do motoryzacji z nowoczesnymi rozwiązaniami. Specjalizujemy się w serwisie pojazdów, modyfikacjach silnika, ulepszaniu zawieszenia oraz kompleksowym przygotowaniu samochodów do zawodów motorsportowych. Zaufaj naszym ekspertom i doświadcz wyjątkowej precyzji i wydajności.",
+  openGraph: {
+    title: "Side2Side | Eksperci w serwisie, tuningu i motorsporcie",
+    description:
+      "Side2Side to profesjonalny warsztat samochodowy, który łączy pasję do motoryzacji z nowoczesnymi rozwiązaniami.",
+    type: "website",
+    locale: "pl-PL",
+    url: "www.side2side.pl",
+    siteName: "Side2Side",
+  },
 };
 
 export default function RootLayout({
