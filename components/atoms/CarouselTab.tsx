79e095a35c0d42d33febd70e86d96639a79be45e
Satorki <sebastian.kedzier@gmail.com>
2024-12-21 20:22:38 +0100

    Project carousel added lazy loading

--- components/atoms/CarouselTab.tsx
+++ components/atoms/CarouselTab.tsx
@@ -23,6 +23,7 @@ const CarouselTab = ({
         alt={title}
         className="object-cover object-center w-full h-full rounded-xl"
         placeholder="blur"
+        loading="lazy"
       />
       {showDescription && (
         <p className="absolute bottom-0 left-0 right-0 rounded-b-xl bg-white p-2  font-bai bg-opacity-80 text-center text-black h-[20%] md:h-[11%] flex items-center justify-center">
